2d44e535bc377f34796b29f3724badaed9a91475
LDR-0 <dlezhayko@gmail.com>
2024-02-02 14:51:40 +0100

    add

--- test.js
+++ test.js
@@ -7,12 +7,8 @@ function findLongestWord(words) {
       }
     }
     return longestWord;
-  }
-  
+}
   console.log(findLongestWord(["Java", "Python", "JavaScript"]));
-  console.log(findLongestWord(["football", "basketball", "tennis"]));
-  console.log(findLongestWord(["sun", "moon", "stars"]));
-
 //2
 function sumUniqueNumbers(numbers) {
     const uniqueNumbers = {};
@@ -25,9 +21,6 @@ function sumUniqueNumbers(numbers) {
     }
     return sum;
 }
-
-console.log(sumUniqueNumbers([1, 2, 3, 2, 4, 3, 5]));
-console.log(sumUniqueNumbers([10, 20, 10, 20, 30]));
 console.log(sumUniqueNumbers([0, -1, -2, -1, -3]));
 
 //3
@@ -42,7 +35,6 @@ function mergeArrays(...arrays) {
     }
     return mergedArray;
 }
-console.log(mergeArrays([1, 2, 3], [4, 5, 6], [7, 8, 9], [2, 3, 4]));
 console.log(mergeArrays([1, 3, 5], [2, 4, 6], [3, 5, 7]));
 
 //4
@@ -55,6 +47,30 @@ function hasProperty(object, propertyName) {
     return false;
 }
   
-console.log(hasProperty({name: "Alice", age: 25}, "age")); // true
-console.log(hasProperty({name: "Bob", country: "France"}, "gender")); // false
-console.log(hasProperty({type: "fruit", color: "red"}, "color"));
+console.log(hasProperty({name: "Alice", age: 25}, "age"));
+
+//5
+function getPropertyValue(object, propertyName) {
+    for (const key in object) {
+      if (Object.prototype.hasOwnProperty.call(object, key) && key === propertyName) {
+        return object[key];
+      }
+    }
+    return undefined;
+}
+  
+console.log(getPropertyValue({name: "Alice", age: 25}, "name"));
+
+//6
+function invertObject(obj) {
+    const inverted = {};
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        inverted[obj[key]] = key;
+      }
+    }
+    return inverted;
+}
+const originalObject1 = { a: 1, b: 2, c: 1 };
+const invertedObject1 = invertObject(originalObject1);
+console.log(invertedObject1);
